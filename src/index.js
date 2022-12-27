df1b93fcf702eb82f237565d286a211a143a51ea
Roman-Huminilovych <103421237+Roman-Huminilovych@users.noreply.github.com>
2022-12-27 15:22:02 +0200

    kk

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
